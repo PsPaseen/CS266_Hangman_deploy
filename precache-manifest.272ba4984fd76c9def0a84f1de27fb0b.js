69872f9b86fe0d967057ee8cc502c6c2c17b50f3
PsPaseen <paseen.san@gmail.com>
2024-04-12 13:53:59 +0700

    Updates

---
+++ precache-manifest.272ba4984fd76c9def0a84f1de27fb0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ccae424f07aa5323450bc185238ade",
+    "url": "/CS266_Hangman_deploy/index.html"
+  },
+  {
+    "revision": "9310f1b817478e5ea282",
+    "url": "/CS266_Hangman_deploy/static/css/main.be5cbd5b.chunk.css"
+  },
+  {
+    "revision": "6273394d175d1bdb07a1",
+    "url": "/CS266_Hangman_deploy/static/js/2.72185cc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CS266_Hangman_deploy/static/js/2.72185cc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9310f1b817478e5ea282",
+    "url": "/CS266_Hangman_deploy/static/js/main.be7ea0a5.chunk.js"
+  },
+  {
+    "revision": "da5c0c236f1f2f3fd2b6",
+    "url": "/CS266_Hangman_deploy/static/js/runtime-main.40a7d88c.js"
+  },
+  {
+    "revision": "8ae86b259961b6433dd9e5971023dcf3",
+    "url": "/CS266_Hangman_deploy/static/media/6n.8ae86b25.png"
+  }
+]);
